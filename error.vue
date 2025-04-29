f93180c1658fcee05270c684dda6b8d06e2371e6
likebocai <likebocai@qq.com>
2025-04-29 16:32:25 +0800

    feat: error界面demo

---
+++ error.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import type { NuxtError } from '#app'
+
+const props = defineProps({
+  error: Object as () => NuxtError
+})
+const route = useRoute();
+const layoutName = ref();
+if(route.fullPath.includes('mobile')) {
+  console.log(route.fullPath)
+  layoutName.value = 'mobile';
+} else {
+  console.log(route.fullPath)
+  layoutName.value = 'pc';
+}
+</script>
+
+<template>
+  <div>
+    <h1>{{ error.statusCode }}</h1>
+    <NuxtLink :to="`/${layoutName}`">返回首页</NuxtLink>
+  </div>
+</template>
